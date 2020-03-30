c29551df2517cb275f9f1663a19552a15392ef8b
zhang <coderzmx@gmail.com>
2020-03-30 23:05:26 +0800

    完成websocket配置。

---
+++ src/main/java/com/ncuedu/bookshopserver/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.ncuedu.bookshopserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @Desc
+ * @Author zhang
+ * @CreateTime 2020/3/30 17:59
+ **/
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
